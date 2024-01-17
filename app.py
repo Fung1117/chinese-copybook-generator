9818b009124cdbe4bdae8f95f2868eece3d6fe89
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2024-01-17 16:09:26 +0800

    updated

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, make_response, request
+from worksheet import generate_worksheet_pdf
+
+app = Flask(__name__)
+
+@app.route('/generate_pdf', methods=['POST'])
+def generate_pdf():
+    if request.method == 'POST':
+        user_input = request.form['text']
+        if user_input:
+            pdf_content = generate_worksheet_pdf()
+        else:
+            pdf_content = generate_worksheet_pdf()
+        response = make_response(pdf_content)
+        response.headers["Content-Type"] = "application/pdf"
+        response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+        return response
+    else:
+        pdf_content = generate_worksheet_pdf()
+        response = make_response(pdf_content)
+        response.headers["Content-Type"] = "application/pdf"
+        response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+        return response 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ worksheet.py
@@ -0,0 +1,53 @@
+import requests
+import pinyin as py
+from pinyin.cedict import translate_word
+from PIL import Image
+from io import BytesIO
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+def get_chinese_word_png(chinese_word):
+    unicode_hex = chinese_word.encode('unicode-escape').decode()[2:]
+    url = f"https://ckc.eduhk.hk/apps/strokes/sequence/200/{unicode_hex}.png"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        image = Image.open(BytesIO(response.content))
+        width, height = image.size
+        return url, width, height
+
+def add_vocabulary(c, vocabulary):
+    w, h = A4
+    max_dimension = 500
+    translation = translate_word(vocabulary)
+
+    for word in vocabulary:
+        
+        c.drawImage('asset/ricegrid.png', 50, h - 60 - 10, width=50, height=50)
+        c.setFillColorRGB(0.7845, 0.7845, 0.7845)
+        c.setFont("baseFont", 50)
+        c.drawString(50, h-60, word)
+
+        pinyin = py.get(word, format='diacritical')
+        c.setFillColorRGB(0, 0, 0)
+        c.setFont("Cousine", 20)
+        c.drawString(50, h - 60 + 45, pinyin)
+        
+        url, width, height = get_chinese_word_png(word)
+        scaling_factor = min(max_dimension / width, max_dimension / height)
+        c.drawImage(url, 50, h - 200, width=int(width * scaling_factor), height=int(height * scaling_factor))
+    
+def generate_worksheet_pdf():
+    pdf_buffer = BytesIO()
+    pdfmetrics.registerFont(TTFont('baseFont', 'asset/font/DFPKaiShuW3-B5.ttf'))
+    pdfmetrics.registerFont(TTFont('Cousine', 'asset/font/Cousine.ttf'))
+    c = canvas.Canvas(pdf_buffer, pagesize=A4)
+
+    add_vocabulary(c, '聰')
+
+    c.save()
+    pdf_data = pdf_buffer.getvalue()
+    pdf_buffer.close()
+    return pdf_data
