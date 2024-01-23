504025ea546df894e16c49f89420939443d0c800
Pun King Fung <99673664+Fung1117@users.noreply.github.com>
2024-01-23 23:17:32 +0800

    updated

--- worksheet.py
+++ worksheet.py
@@ -1,20 +1,36 @@
 import os
 import requests
+import numpy as np
 import pinyin as py
 from pinyin.cedict import translate_word
-from PIL import Image
+from PIL import Image, ImageOps
 from io import BytesIO
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
+def change_color(image_path):
+    try:
+        image = Image.open(image_path)
+        image = image.convert("RGB")
+        image_array = np.array(image)
+        target_color = (0, 0, 0)
+        target_channels = np.array(target_color)
+        color_mask = np.all(image_array == target_channels, axis=2)
+        image_array[color_mask] = [200, 200, 200]
+        modified_image = Image.fromarray(image_array)
+        modified_image.save(image_path)
+        print("Color changed to red and saved successfully.")
+    except IOError:
+        print("Unable to change the color to red.")
+
 def get_chinese_word_png(chinese_word):
     unicode_hex = chinese_word.encode('unicode-escape').decode()[2:]
     image_path = f"assets/strokes/sequence/{unicode_hex}.png"
 
     if os.path.exists(image_path):
-        image = Image.open(image_path).convert("L")
+        image = Image.open(image_path)
         width, height = image.size
         return image_path, width, height
 
@@ -25,7 +41,8 @@ def get_chinese_word_png(chinese_word):
         os.makedirs(os.path.dirname(image_path), exist_ok=True)
         with open(image_path, 'wb') as f:
             f.write(response.content)
-        image = Image.open(BytesIO(response.content)).convert("L")
+        image = Image.open(BytesIO(response.content))
+        change_color(image_path)
         width, height = image.size
         return image_path, width, height
 
@@ -51,9 +68,11 @@ def add_vocabulary(c, vocabulary, pdf_height):
         c.setFont("baseFont", 50)
         c.drawString(50 * index, h - pdf_height - 45, word)
 
-        url, width, height = get_chinese_word_png(word)
+
+    for index, word in enumerate(vocabulary, start=1):
+        image_path, width, height = get_chinese_word_png(word)
         scaling_factor = min(max_dimension / width, max_dimension / height)
-        c.drawImage(url, 50, h - pdf_height - 40 * index - 100, width=int(width * scaling_factor), height=int(height * scaling_factor))
+        c.drawImage(image_path, 50, h - pdf_height - 45 * index - 60, width=int(width * scaling_factor), height=int(height * scaling_factor))
 
     return pdf_height
 
@@ -67,11 +86,11 @@ def generate_worksheet_pdf():
     c = canvas.Canvas(pdf_buffer, pagesize=A4)
 
     pdf_height = 50 
-    vocabularies = ['你好']
-    # vocabularies = ['明聰', '聰明', '做']
+    # vocabularies = ['你好']
+    vocabularies = ['明聰', '聰明', '做']
     for vocabulary in vocabularies:
         add_vocabulary(c, vocabulary, pdf_height)
-        pdf_height += 400 
+        break
 
     c.save()
     pdf_data = pdf_buffer.getvalue()
